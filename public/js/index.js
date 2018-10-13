5022e322332a6a01b04e5d7943d4c3e4ece6497c
Jesus Arciniega <dev.jesusarc@gmail.com>
2018-10-12 23:12:18 -0600

    backup

--- public/js/index.js
+++ public/js/index.js
@@ -10,10 +10,37 @@ document.addEventListener('DOMContentLoaded', function() {
   var sidenavInstance = M.Sidenav.init(sidenav);
   //Activate inputs
   M.updateTextFields();
+  getProjects();
 });
 
 
 //Show new project form n hide sidenav
 function show_form(){
   document.getElementById('new_project').style.display = 'block';
+}
+
+//Get proyects list
+function getProjects(){
+  var projectsRef = db.collection("projects");
+  
+  projectsRef.onSnapshot(function(doc) {
+        document.getElementById("projects_list").innerHTML = '';
+        var source = doc.metadata.hasPendingWrites ? "Local" : "Server";
+        console.log(source);
+        
+        doc.forEach(doc =>{
+        var ul = document.getElementById("projects_list");
+        var li = document.createElement("li");
+        var div = document.createElement("div");
+        var data = '<div class="collapsible-header">'+doc.data().name+' <span class="badge">'+doc.data().deadline+'</span></div>';
+        data += '<div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>';
+        
+        div.innerHTML = data;
+        li.appendChild(div);
+        ul.appendChild(li);
+        data = '';
+        
+    });
+    document.getElementById('loading').style.display = 'none';
+  });
 }
