56befc7e948b5a5aa7a9d64d1eb1dc89bdcbfb04
Jesus Arciniega <dev.jesusarc@gmail.com>
2018-10-12 21:54:36 -0600

    Design adjustments

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
+  //Hide form
+  var link = document.getElementById('new_project');
+  link.style.display = 'none';
+  //Activate collapsible
   var elems = document.querySelectorAll('.collapsible');
-  var instances = M.Collapsible.init(elems);
+  M.Collapsible.init(elems);
+  //Activate sidenav
+  var sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+  //Activate inputs
+  M.updateTextFields();
 });
