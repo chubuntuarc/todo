104f8ec13fad6470f07bc5a3a4bdb8879179d2af
Jesus Arciniega <dev.jesusarc@gmail.com>
2018-10-12 22:16:49 -0600

    updates

--- public/js/index.js
+++ public/js/index.js
@@ -4,10 +4,16 @@ document.addEventListener('DOMContentLoaded', function() {
   link.style.display = 'none';
   //Activate collapsible
   var elems = document.querySelectorAll('.collapsible');
-  M.Collapsible.init(elems);
+  var collapsibleInstance = M.Collapsible.init(elems);
   //Activate sidenav
   var sidenav = document.querySelectorAll('.sidenav');
-  M.Sidenav.init(sidenav);
+  var sidenavInstance = M.Sidenav.init(sidenav);
   //Activate inputs
   M.updateTextFields();
 });
+
+
+//Show new project form n hide sidenav
+function show_form(){
+  document.getElementById('new_project').style.display = 'block';
+}
